1f9dbe5a5f0ff84a5fec85599f5ec737a472bcce
Khalil Ahmed <134753862+Khalil20star@users.noreply.github.com>
2024-05-19 11:20:06 +0300

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -6,7 +6,7 @@ def connect():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Ka@787898",
+            password="*******",
             database="library_system"
         )
         if connection.is_connected():
